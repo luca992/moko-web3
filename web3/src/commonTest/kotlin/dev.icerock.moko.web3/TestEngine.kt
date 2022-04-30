75edfcdde98b5f5a019e9aeb6abf8c4a85a9e71b
Luca Spinazzola <mspinluca@gmail.com>
2022-04-30 15:16:07 -0400

    ktor import fixes.

--- web3/src/commonTest/kotlin/dev.icerock.moko.web3/TestEngine.kt
+++ web3/src/commonTest/kotlin/dev.icerock.moko.web3/TestEngine.kt
@@ -4,18 +4,10 @@
 
 package dev.icerock.moko.web3
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.mock.MockEngine
-import io.ktor.client.engine.mock.MockEngineConfig
-import io.ktor.client.engine.mock.MockRequestHandleScope
-import io.ktor.client.engine.mock.respond
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
-import io.ktor.client.request.HttpRequest
-import io.ktor.client.request.HttpRequestData
-import io.ktor.client.request.HttpResponseData
-import io.ktor.content.TextContent
+import io.ktor.client.*
+import io.ktor.client.engine.mock.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
 
 // When new test added, it should be tested with this real client,
 // And then mocked request should be added to client below,
